8291c441d854c42ba74331337a57e1b46b2f122b
Srrdrr <anvarovsardor926@gmail.com>
2025-03-14 21:29:28 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+alert(`Sizda hozir random sonlar chqishi boshlaydi😉`)
+alert(`Random orqali chiqadigan son: 22`)
+alert(`Bu sonni 2ga kopaytirmoqchi va bolmoqchimiz !`)
+alert(`Random orqali chiqadigan son: 22 edi\n Biz uni kopaytirib 44 sonini chiqardik \n va shu sonni 2ga bolib: 11 sonini chiqardik \n va shi sonni 3ga bolgandagi qoldiq: 1 sonini chiqardik`)
+
+
+
+alert(`Sizda hozir random sonlar chqishi boshlaydi😉`)
+alert(`Random orqali chiqadigan son: 55`)
+alert(`Bu sonni 2ga kopaytirmoqchi va bolmoqchimiz !`)
+alert(`Random orqali chiqadigan son: 55 edi\n Biz uni kopaytirib 110 sonini chiqardik \n va shu sonni 2ga bolib: 27.5 sonini chiqardik \n va shi sonni 3ga bolgandagi qoldiq: 1 sonini chiqardik`)
